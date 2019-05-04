e6fee4308a3630c02dc41684a6275690932d7e61
doug <drehnstrom@yahoo.com>
2019-05-04 15:28:22 -0400

    Added DotNet App

---
+++ dotnet-app/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            // This function was edited to support Google Cloud Run
+            // The port defaults to 8080
+            // Cloud Run set a "PORT" env variable which overrides 8080
+            string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            string url = String.Concat("http://0.0.0.0:", port);
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseUrls(url);
+        }
+    }
+}
